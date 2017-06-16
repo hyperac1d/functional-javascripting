b7eae87c07479ea2b8a2da9afdd5c2117765d9e7
hyperac1d <dlastgamer18@gmail.com>
2017-06-16 12:04:09 +0800

    initial commit

---
+++ Exercises/Hello-World.js
@@ -0,0 +1,6 @@
+function upperCaser(input){
+	var str = String(input);
+	var get = str.toUpperCase();
+	return get;
+}
+module.exports = upperCaser;
---
+++ Exercises/High Order Function.js
@@ -0,0 +1,7 @@
+/* High Order Function*/
+ function repeat (operation, num) {
+ 	if (num <= 0) 
+ 		return operation()
+  	return repeat(operation, --num)
+ }
+ module.exports = repeat
